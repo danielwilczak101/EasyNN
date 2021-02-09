639616ff0942d4d6e0a795ea3dc57fad073e5964
danielwilczak <danielwilczak101@gmail.com>
2021-02-08 22:11:59 -0500

    I added the main Neuralnetwork class.

--- EasyNN/EasyNN.py
+++ EasyNN/EasyNN.py
@@ -1,6 +1,5 @@
-import import_this
-
-class NN:
+class NerualNetwork:
+    """This is the main nerual network class that will be used by the user. """
 
     def say_hello(self):
         print("Hello NN World!")
