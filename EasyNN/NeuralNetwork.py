b4f63a929d2f2ae7ee3a4944478405228a45d553
SimpleArt <71458112+SimpleArt@users.noreply.github.com>
2021-02-20 10:55:31 -0500

    Scalar points and training

--- EasyNN/NeuralNetwork.py
+++ EasyNN/NeuralNetwork.py
@@ -1,3 +1,7 @@
+"""
+Generic Neural Network module. Use NeuralNetwork() to create a neural network.
+"""
+
 # Special import which allows type hints
 # involving the class itself to be inside.
 from __future__ import annotations
@@ -25,6 +29,15 @@ class NeuralNetwork:
     nodes: Sequence[NeuralNode]
     """The NeuralNode's stored in the neural network."""
 
+    input_nodes: Sequence[NeuralNode]
+    """Nodes which attain values through input."""
+
+    hidden_nodes: Sequence[NeuralNode]
+    """Nodes which are not interacted with externally."""
+
+    output_nodes: Sequence[NeuralNode]
+    """Nodes used to get outputs."""
+
     def __init__(
             self,
             nodes: Dict[int, List[int]],
@@ -44,8 +57,11 @@ class NeuralNetwork:
 
         # Create connections between nodes and assign weights
         for node, node_indexes in zip(self.nodes, nodes):
-            node.connected_nodes = [self.nodes[i] for i in node_indexes]
-            node.weights = Point((len(node_indexes),))
+            node.input_nodes = [self.nodes[i] for i in node_indexes]
+            node.input_weights = [Point() for _ in node_indexes]
+            for i, weight in zip(node_indexes, node.input_weights):
+                self.nodes[i].output_nodes.append(node)
+                self.nodes[i].output_weights.append(weight)
 
         # Assign activation functions
         for key, func in activation.items():
@@ -54,7 +70,7 @@ class NeuralNetwork:
         # Collect input nodes i.e. connecting to nothing
         self.input_nodes = []
         for node in self.nodes:
-            if len(node.connected_nodes) == 0:
+            if len(node.input_nodes) == 0:
                 self.input_nodes.append(node)
             else:
                 break
@@ -88,6 +104,11 @@ class NeuralNetwork:
         # Collect hidden nodes
         self.hidden_nodes = self.nodes[len(self.input_nodes):(len(self.nodes)-len(self.output_nodes))]
 
+        # Randomize weights
+        for node in self.output_nodes + self.hidden_nodes:
+            for weight in node.input_weights:
+                weight.randomize()
+
 
     def __call__(
             self,
@@ -102,45 +123,77 @@ class NeuralNetwork:
         and return the node.value's from the output_nodes.
         """
 
-        # Cast to numpy array for reshaping
+        # cast to numpy array
         if not isinstance(input_values, np.ndarray):
             input_values = np.array(input_values)
 
-        # Get the output shape and flatten the input
+        # get the output shape and flatten the input
         output_shape = output_shape or input_values.shape
         input_values = input_values.flatten()
 
         if len(input_values) > len(self.input_nodes):
-            raise ValueError("Too many inputs, expected at most {len(self.input_nodes)} nodes")
+            raise ValueError(
+                f"Too many inputs, expected at most {len(self.input_nodes)} nodes")
 
-        # Fill in the inputs followed by repeated pads
-        input_values = chain(input_values, repeat(pad, len(self.input_nodes) - len(input_values)))
+        # fill in the inputs followed by repeated pads
+        input_values = chain(
+            input_values,
+            repeat(pad, len(self.input_nodes) - len(input_values))
+        )
 
         for node, value in zip(self.input_nodes, input_values):
-            node.value.points = [value]
+            node.value.points = value
 
-        # Perform feed-forward propagation
-        for node in chain(self.hidden_nodes, self.output_nodes):
+        # perform feed-forward propagation over the hidden then output nodes
+        for node in self.hidden_nodes + self.output_nodes:
             node.feed_forward()
 
-        # Return the output in the original shape
-        return np.array([
-            node.value.points
-            for node in self.output_nodes
-        ]).reshape(output_shape)
+        # return the output in the original shape
+        return np.reshape(
+            [node.value.points for node in self.output_nodes],
+            output_shape,
+        )
 
 
     def train(
             self,
-            input_data: TensorType,
-            output_data: TensorType,
+            input_data: Sequence[TensorType],
+            output_data: Sequence[TensorType],
+            epochs: int = 1000,
     ):
         """
         Train is used to take a data set and train it based on
         an input and output.
         """
 
-        pass
+        # flatten the data
+        flatten = lambda tensor: np.reshape(tensor, (len(tensor),))
+        input_data = map(flatten, input_data)
+        output_data = map(flatten, output_data)
+
+        # merge the data
+        data = np.array(list(zip(input_data, output_data)))
+
+        for i in range(epochs):
+
+            # shuffle and loop through the data
+            np.random.shuffle(data)
+            for x, y in data:
+
+                # insert x into the model
+                self(x)
+
+                # adjust output derivatives from expected values
+                for node, expected_value in zip(self.output_nodes, y):
+                    node.value.derivatives = 2*(node.value.points - expected_value)
+
+                # back-propagate the derivative
+                for node in reversed(self.nodes):
+                    node.back_propagate()
+
+                # update parameters based on derivatives
+                for node in self.nodes:
+                    node.optimize()
 
 
 class NeuralNode:
@@ -154,7 +207,20 @@ class NeuralNode:
     #=======================#
 
     value: Point
-    """Scalar representing the value in the node."""
+    """
+    Scalar representing the value in the node
+    after the activation function is applied.
+    
+    value = activation(prevalue)
+    """
+
+    prevalue: Point
+    """
+    Scalar representing the value in the node
+    before the activation function is applied.
+    
+    value = np.dot(weights, previous_nodes.value) + bias
+    """
 
     bias: Point
     """Scalar representing the bias of the node."""
@@ -165,32 +231,39 @@ class NeuralNode:
     Defaulted to the identity function.
     """
 
-    connected_nodes: Sequence[NeuralNode]
-    """Vector of all connected nodes."""
+    input_nodes: Sequence[NeuralNode]
+    """Vector of input nodes."""
 
-    weights: Point
-    """Vector of weights for connected nodes."""
+    input_weights: Sequence[Point]
+    """Vector of weights for input nodes."""
+
+    output_nodes: Sequence[NeuralNode]
+    """Vector of output nodes."""
+
+    output_weights: Sequence[Point]
+    """Vector of weights for output nodes."""
 
 
     def __init__(
         self,
         activation: Callable[[float], float] = lambda x: x,  # no change
-        connected_nodes: Sequence[NeuralNode] = tuple(),
     ):
         """
-        Initialize with float (random by default),
+        Initialize with float (random),
         activation function (no change by default),
-        and connected nodes (no connections by default)
+        and connected nodes (no connections),
         and random weights.
-        Initialize delta's to 0.
         """
 
         # node attributes
-        self.value = Point((1,))
-        self.bias = Point((1,))
+        self.value = Point()
+        self.prevalue = Point()
+        self.bias = Point()
         self.activation = activation
-        self.connected_nodes = connected_nodes
-        self.weights = Point((len(connected_nodes),))
+        self.input_nodes = []
+        self.input_weights = []
+        self.output_nodes = []
+        self.output_weights = []
 
 
     #=========#
@@ -199,20 +272,49 @@ class NeuralNode:
 
 
     def feed_forward(self):
-        """Uses connected nodes to compute self.value."""
+        """Uses input nodes to compute self.value."""
 
-        self.value.points = [self.activation(
-            np.dot(
-                self.weights.points,
-                [node.value.points for node in self.connected_nodes]
-            )
-        )]
+        self.prevalue.points = sum(
+            weight.points * node.value.points
+            for weight, node
+            in zip(self.input_weights, self.input_nodes)
+        )
+
+        self.value.points = self.activation(self.prevalue.points)
 
 
     def back_propagate(self):
-        """
-        Modify change attributes to more closely match value_change.
-        Modify nodes.value_change to more closely match value_change.
-        Modify respective attributes based on their attr_change.
-        """
-        pass
+        """Updates derivatives of all parameters."""
+
+        # d(loss) / d(value)
+        if len(self.output_nodes) > 0:
+            self.value.derivatives = sum(
+                weight.points * node.value.derivatives
+                for weight, node
+                in zip(self.output_weights, self.output_nodes)
+            )
+
+        # d(activation) / d(prevalue)
+        try:
+            derivative = self.activation.derivative(self.prevalue.points)
+        except AttributeError:
+            x = self.prevalue.points
+            dx = 1e-8 * (1 + abs(x))
+            derivative = (self.activation(x+dx) - self.activation(x-dx)) / (2*dx)
+
+        # d(loss) / d(prevalue) = d(loss) / d(bias)
+        self.prevalue.derivatives = derivative * self.value.derivatives
+        self.bias.derivatives = derivative * self.value.derivatives
+
+        # d(loss) / d(weight)
+        for weight, node in zip(self.input_weights, self.input_nodes):
+            weight.points = self.prevalue.derivatives * node.value.points
+
+
+    def optimize(self):
+        """Updates points based on their derivatives."""
+
+        self.bias.optimize()
+
+        for weight in self.input_weights:
+            weight.optimize()
