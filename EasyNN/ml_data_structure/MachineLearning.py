b8b7c7ed385214020c2fc9fcc63a836a842f0af1
SimpleArt <71458112+SimpleArt@users.noreply.github.com>
2021-03-06 12:40:32 -0500

    Replaced Tensors with Point

--- EasyNN/ml_data_structure/MachineLearning.py
+++ EasyNN/ml_data_structure/MachineLearning.py
@@ -1,3 +1,4 @@
+from Point import Point
 from Tensor import TensorLike
 from Optimizer import Optimizer
 
@@ -11,37 +12,43 @@ class MachineLearning:
     iteration: int
     """Number of times self.optimize() was called."""
 
-    items: TensorLike
-    """2D array of [values, derivatives], holding all parameter references."""
+    items: Point
+    """Tensor in the form of [values, derivatives], holding all parameter references."""
 
     optimizer: Optimizer
     """Algorithm for optimizing values."""
 
-    def __init__(self, items: TensorLike, optimizer: Optimizer):
+
+    def __init__(self, items: Point, optimizer: Optimizer):
         """Initialize a ML object with values, derivatives, and an optimizer."""
         self.iteration = 0
         self.items = items
         self.optimizer = optimizer
+
 
     def optimize(self):
         """Optimize the values given the values and derivatives, using the optimizer."""
         self.iteration += 1
         self.optimizer.update(self.iteration, self.values, self.derivatives)
 
+
     @property
     def values(self) -> TensorLike:
         """The weights and biases of the model."""
         return self.items[0]
+
 
     @values.setter
     def values(self, input_values: TensorLike):
         """Assign values while preserving references."""
         self.items[0] = input_values
 
+
     @property
     def derivatives(self) -> TensorLike:
         """The derivatives of the weights and biases of the model."""
         return self.items[1]
+
 
     @derivatives.setter
     def derivatives(self, input_derivatives: TensorLike):
