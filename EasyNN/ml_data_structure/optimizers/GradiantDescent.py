f16a0dec7b8fed4ec9406219a82713364c37e4ba
SimpleArt <71458112+SimpleArt@users.noreply.github.com>
2021-03-19 12:35:35 -0400

    Using Point
    update method now using Point, and comments rewritten.

--- EasyNN/ml_data_structure/optimizers/GradiantDescent.py
+++ EasyNN/ml_data_structure/optimizers/GradiantDescent.py
@@ -1,16 +1,15 @@
 import Optimizer
+from Point import Point
+
 
 class GradiantDescent(Optimizer):
-    """ Just your super simple GO STRAIGHT DOWN optimizer """
+    """Basic Gradient Descent optimizer, using only a learning rate and derivatives."""
     
-    def __init__(self, learningRate):
-        """ requires a learning rate"""
-        self.learningRate = learningRate
+    def __init__(self, learning_rate: float = 0.1):
+        """Requires a learning rate."""
+        self.learning_rate = learning_rate
 
         
-    def update(self, iteration: int, values: TensorLikem derivatives: TensorLike):
-
-        # Im not sure if this is necessary
-        
-        for (value, derivative) in zip(values, derivatives):
-            value += learningRate * derivative
+    def update(self, iteration: int, items: Point):
+        """Subtract the derivatives times the learning rate from the values."""
+        items.values -= self.learning_rate * items.derivatives
