91905278ba2bda1e7ff7f995446cf5f89a2f065b
SimpleArt <71458112+SimpleArt@users.noreply.github.com>
2021-03-24 17:14:02 -0400

    Standardized test case format

--- tests/sphereTest.py
+++ tests/sphereTest.py
@@ -1,23 +1,50 @@
 import ml_optimizer_test as opt_tester
 import os, sys
+from typing import Tuple
 #no bounds
 
-def sphereFunction(x : float, y : float):
-    return (x ** 2) + (y ** 2)
 
-def sphere_derivative(x: float, y : float):
-    return ( (2 * x), (2 * y))
+class OptimizerTest:
+    """Interface for optimizer test cases."""
 
-def runTest(outputDir:str='output'):
+    dimensions: int = 2
+
+    @staticmethod
+    def func(*args: Tuple[float, ...]):
+        pass
+
+    @staticmethod
+    def derivative(*args: Tuple[float, ...]):
+        pass
+
+
+class Sphere(OptimizerTest):
+    """Test case for sphere function."""
+
+    @staticmethod
+    def func(x: float, y: float):
+        return (x**2 + y**2) / 100
+
+    @staticmethod
+    def derivative(x: float, y : float):
+        return 0.02*x, 0.02*y
+
+
+def runTest(test_case: OptimizerTest, outputDir: str = 'output'):
     #grab, and create directory if it doesnt exist
-    directory=os.path.isdir(outputDir)
+    directory = os.path.isdir(outputDir)
     if not os.path.isdir(outputDir):
         os.makedirs(outputDir)
-    outputFile="sphere"
-    opt_tester.plot_3D_test(file_name=outputFile,
-                 func=sphereFunction,
-                 derivative=sphere_derivative)
-    
-                 
+
+    if test_case.dimensions == 2:
+        opt_tester.plot_3D_test(
+            file_name=test_case.__name__,
+            func=test_case.func,
+            derivative=test_case.derivative,
+        )
+
+    else:
+        raise ValueError("Only test cases with 2 inputs are implemented.")
+
 if __name__ == '__main__':
-    runTest()
+    runTest(Sphere)
