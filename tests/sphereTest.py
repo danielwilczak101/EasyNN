4efe3b8d23075241c8bde9cd1ac48aa03136699f
SimpleArt <71458112+SimpleArt@users.noreply.github.com>
2021-03-25 00:18:34 -0400

    Used numpy, fixed test cases, added multiprocessing

--- tests/sphereTest.py
+++ tests/sphereTest.py
@@ -1,17 +1,22 @@
 import ml_optimizer_test as opt_tester
-import os, sys, math
+import os, sys
+from multiprocessing import Pool
 from typing import Optional, Tuple
+from numpy import pi, exp, sin, cos, sqrt
 #no bounds
 
 
 class OptimizerTest:
     """Interface for optimizer test cases."""
-
     dimensions: int = 2
     bounds: Optional[Tuple[float, float]] = (-10, +10)
     initial_point: Optional[Tuple[float, ...]] = None
     iterations: int = 1000
-    learning_rate: float = 0.1
+    learning_rate: float = 3e-3
+    plot_density: int = 25
+
+    def __init__(self, name = ""):
+        self.name = type(self).__name__ + str(name)
 
     @staticmethod
     def func(*args: Tuple[float, ...]):
@@ -23,7 +28,8 @@ class OptimizerTest:
 
 
 class Sphere(OptimizerTest):
-    """Test case for sphere function."""
+    """Test case for the sphere function."""
+    learning_rate = 0.1
 
     @staticmethod
     def func(x: float, y: float):
@@ -35,28 +41,36 @@ class Sphere(OptimizerTest):
 
     
 class Rastrigin(OptimizerTest):
-    """Test case for sphere function."""
+    """Test case for the Rastrigin function."""
 
     @staticmethod
     def func(x: float, y: float):
-        return 20 + (x ** 2) - 10 * math.cos(2 * math.pi * x) + (y ** 2) - 10 * math.cos(2 * math.pi *y)
+        f = lambda t: t**2 + 10 * (1 - cos(2*pi*t))
+        return f(x) + f(y)
 
     @staticmethod
     def derivative(x: float, y : float):
-        return ((2 * x - 20 * math.pi * math.sin(2 * math.pi * x)), (2 * y - 20 * math.pi * math.sin(2 * math.pi * y)))
+        f = lambda t: 2*t + 20 * pi * sin(2*pi*t)
+        return (f(x), f(y))
+
 
 class Ackley(OptimizerTest):
-    """Test case for sphere function."""
+    """Test case for the Ackley function."""
 
     @staticmethod
     def func(x: float, y: float):
-        return -20 * math.e ** (-0.2 * math.sqrt( 0.5 * ( (x ** 2) + (y ** 2) )))
+        term1 = -20 * exp(-0.2 * sqrt(0.5 * (x**2 + y**2)))
+        term2 = -exp(0.5 * (cos(2*pi*x) + cos(2*pi*y)))
+        return term1 + term2 + exp(1) + 20
 
     @staticmethod
-    def derivative(x: float, y : float):
-        return ( ((4 * x) / math.sqrt(2 * ( (x **2) + (y ** 2)) ) * math.e ** (-0.2 * math.sqrt(0.5 * ( (x ** 2) + (y ** 2))))),
-                 ((4 * y) / math.sqrt(2 * ( (x **2) + (y ** 2)) ) * math.e ** (-0.2 * math.sqrt(0.5 * ( (x ** 2) + (y ** 2))))) )
-    
+    def derivative(x: float, y: float):
+        sqrtsquares = sqrt(0.5 * (x**2 + y**2))
+        term1 = 4 / sqrtsquares * exp(-0.2 * sqrtsquares)
+        term2 = pi * exp(0.5 * (cos(2*pi*x) + cos(2*pi*y)))
+        f = lambda t: t * term1 + sin(2*pi*t) * term2
+        return (f(x), f(y))
+
 
 def runTest(test_case: OptimizerTest, outputDir: str = 'output'):
     #grab, and create directory if it doesnt exist
@@ -64,7 +78,7 @@ def runTest(test_case: OptimizerTest, outputDir: str = 'output'):
     if not os.path.isdir(outputDir):
         os.makedirs(outputDir)
 
-    filePath = outputDir + "/" + test_case.__name__
+    filePath = outputDir + "/" + test_case.name
     if test_case.dimensions == 2:
         opt_tester.plot_3D_test(
             file_name=filePath,
@@ -74,12 +88,12 @@ def runTest(test_case: OptimizerTest, outputDir: str = 'output'):
             initial_point=test_case.initial_point,
             iterations=test_case.iterations,
             learning_rate=test_case.learning_rate,
+            plot_density=test_case.plot_density,
         )
 
     else:
         raise ValueError("Only test cases with 2 inputs are implemented.")
 
 if __name__ == '__main__':
-    runTest(Sphere)
-    runTest(Rastrigin)
-    runTest(Ackley)
+    with Pool(3) as p:
+        p.map(runTest, [Sphere(), Rastrigin(), Ackley()])
