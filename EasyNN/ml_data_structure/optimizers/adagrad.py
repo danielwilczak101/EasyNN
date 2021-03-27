c4a825ac70e5c9928528af8768c47f214f22a31d
SimpleArt <71458112+SimpleArt@users.noreply.github.com>
2021-03-27 09:12:15 -0400

    Cleaned up adagrad

--- EasyNN/ml_data_structure/optimizers/adagrad.py
+++ EasyNN/ml_data_structure/optimizers/adagrad.py
@@ -1,26 +1,24 @@
+from numpy import sqrt
+from EasyNN.ml_data_structure.Tensor import TensorLike
 from EasyNN.ml_data_structure.opimizers.Optimizer import Optimizer
 
-class StochasticAdagrad(Optimizer):
-    """Adagrad method with stochastic learning rate."""
 
+class Adagrad(Optimizer):
+    """Adagrad method."""
     learning_rate: float
     decay: float
     epsilon: float
+    squares: TensorLike
 
-    def __init__(self, learning_rate: float = 1.0, decay: float = 0.0, epsilon: float = 1e-7):
+    def __init__(self, *, learning_rate: float = 1.0, decay: float = 0.0, epsilon: float = 1e-7):
         """Initialize using learning rate, decay rate, and epsilon."""
         self.learning_rate = learning_rate
         self.decay = decay
         self.epsilon = epsilon
-        self.cache = None
+        self.squares = 0
 
     def update(self, iteration: int, items: Point):
         """Rescale the weight of each component in the derivatives using an accumulating cache."""
-        
-        # Initialize the cache if necessary
-        if self.cache is None:
-            self.cache = np.zeros_like(items.values)
 
-        self.cache += items.derivatives ** 2
-        items.values -= (self.learning_rate / (1.0 + self.decay * iteration)) * items.derivatives / (np.sqrt(self.cache) + self.epsilon)
-
+        self.squares += items.derivatives ** 2
+        items.values -= (self.learning_rate / (1.0 + self.decay * iteration)) * items.derivatives / (np.sqrt(self.squares) + self.epsilon)
