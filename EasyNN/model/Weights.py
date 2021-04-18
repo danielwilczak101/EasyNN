00d704213b9133b072afe6b4deeba68990b8e748
SimpleArt <71458112+SimpleArt@users.noreply.github.com>
2021-04-18 16:10:45 -0400

    Fixed Weight.parameters shape and simplified math

--- EasyNN/model/Weights.py
+++ EasyNN/model/Weights.py
@@ -10,10 +10,16 @@ from EasyNN.Batch import MiniBatch
 class Weights(Model):
     """Generic model API."""
     parameters: np.ndarray
-    def __init__(self, num_inputs, num_neurons):
-        self.parameters = np.random.random_sample((2, num_neurons, num_inputs))
-        self.num_inputs = num_inputs
-        self.num_neurons = num_neurons
+
+
+    def __init__(self, inputs: int, outputs: int) -> None:
+        max_weight = np.sqrt(6 / (inputs + outputs))
+        self.parameters = np.vstack([
+            np.random.uniform(-max_weight, max_weight, inputs * outputs),
+            np.zeros(inputs * outputs),
+        ])
+        self.shape = (outputs, inputs)
+
 
     def __call__(self, values: ArrayLike) -> np.ndarray:
         """
@@ -26,21 +32,12 @@ class Weights(Model):
 
         Returns
         -------
-        np.ndarray
-            A numpy array representing the output, representing the result
-            of the model on the given input.
+        out : np.ndarray
+            Weights.matrix @ values
         """
-        self.inputs = values #need to save for back propogation
-        if len(np.array(values).shape) == 2: #save number of batches for later
-            self.numBatches = np.array(values).shape[0]
-        else:
-            self.numBatches = 1
-            
-        fwd = np.dot(values, np.array(self.values).T)
-        if self.numBatches == 1:
-            return fwd.flatten() #flatten again
-        else:
-            return fwd
+        self.inputs = np.array(values, copy=False)
+        return self.matrix @ self.inputs
+
 
     def backpropagate(self, derivatives: ArrayLike) -> np.ndarray:
         """
@@ -53,16 +50,28 @@ class Weights(Model):
 
         Returns
         -------
-        np.ndarray
-            A numpy array representing the output derivative, representing
-            how the previous input should be changed to get the desired
-            change in output when using the model.
+        out : np.ndarray
+            Weights.matrix.T @ derivatives
         """
-        self.derivatives = np.dot(np.matrix(self.inputs).T, np.matrix(derivatives)).T #im too lazy to figure simplify
-        back = np.dot(derivatives, np.array(self.values))
+        derivatives = np.array(derivatives, copy=False)
 
-        if self.numBatches == 1:
-            return back.flatten()
+        if derivatives.ndim == 1:
+            self.derivatives = np.outer(derivatives, self.inputs).flatten()
+
         else:
-            return back
+            self.derivatives = (derivatives.T @ self.inputs).flatten()
+            self.derivatives /= len(derivatives)
 
+        return self.matrix.T @ derivatives
+
+
+    @property
+    def matrix(self) -> np.ndarray:
+        """Property for interpreting Weights.values as a matrix."""
+        return self.values.reshape(self.shape)
+
+
+    @matrix.setter
+    def matrix(self, values: ArrayLike) -> None:
+        """Replaces the values in the shape of the matrix."""
+        self.matrix[...] = values
