788986fceaa0238d6edc35b04af769624504fa29
SimpleArt <71458112+SimpleArt@users.noreply.github.com>
2021-04-28 13:19:56 -0400

    Minor change

--- EasyNN/model/activation/ReLU.py
+++ EasyNN/model/activation/ReLU.py
@@ -9,7 +9,7 @@ class ReLU(Activation, Model):
 
     def __call__(self, values: ArrayLike) -> np.ndarray:
         self.input = np.array(values, copy=False)
-        return np.maximum(values, 0)
+        return np.maximum(self.input, 0)
 
     def backpropagate(self, derivatives: ArrayLike) -> np.ndarray:
         return np.where(self.input > 0, derivatives, 0)
