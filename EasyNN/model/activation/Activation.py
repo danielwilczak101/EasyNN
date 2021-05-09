72992442e877fe33964ff70fe6e598da541a780d
danielwilczak <danielwilczak101@gmail.com>
2021-05-09 16:21:19 -0400

    Refresh

--- EasyNN/model/activation/Activation.py
+++
@@ -1,6 +0,0 @@
-from EasyNN.model.Model import Model
-
-
-class Activation(Model):
-    """Generic activation model API."""
-    pass
--- EasyNN/model/non_dense/Optimizer.py
+++
@@ -1,9 +0,0 @@
-from Tensor import TensorLike
-
-
-class Optimizer:
-    """Base Optimizer class for machine learning algorithms."""
-
-    def update(self, iteration: int, values: TensorLike, derivatives: TensorLike):
-        """Updates values using other parameters."""
-        raise NotImplemented("Requires Optimizer.update(...) to be overriden.")
