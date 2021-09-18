17cdb675abd5efa4ccdec1d4983d3e8dbab76268
SimpleArt <71458112+SimpleArt@users.noreply.github.com>
2021-09-18 13:39:41 -0400

    Cleaned up type-hints

--- EasyNN/batch/abc.py
+++ EasyNN/batch/abc.py
@@ -1,23 +1,54 @@
 from __future__ import annotations
 from abc import ABC, abstractmethod
 import numpy as np
-from typing import Iterator
-from EasyNN.typing import ArrayND
+import typing import Iterable, Iterator
+from EasyNN.typing import ArrayND, Sample
+
+if sys.version_info < (3, 9):
+    class Iterator(Generic
+
+def validate(data: Sample) -> None:
+    """Type-check the training/testing/validation data."""
+    if not isinstance(training, tuple):
+        raise TypeError(f"training must be a tuple, not {type(training).__name__}")
+    elif not all(isinstance(x, ArrayND) for x in training):
+        raise TypeError("training must be a tuple of ND-Arrays")
+
+def split(data: Sample, percent: float) -> tuple[Sample, Sample]:
+    """Split the training data up to fill in for testing/validation data."""
+    return = (
+        tuple([x[:int(test_percent * len(training))] for x in training]),
+        tuple([x[int(test_percent * len(training)):] for x in training]),
+    )
 
 
 class Dataset:
     """Class for storing datasets and looping over multiple arrays together."""
     _indexes: Iterator[Union[int, slice, tuple[int]]]
-    data: tuple[ArrayND, ...]
+    data: Sample
+    sample: Sample
 
-    def __init__(self: Dataset, *data: ArrayND, indexes: Iterator[Union[int, slice, tuple[int, ...]]]) -> None:
+    def __init__(self: Dataset, *data: ArrayND, indexes: Iterable[Union[int, slice, tuple[int, ...]]]) -> None:
         self.data = data
-        self._indexes = indexes
+        self._indexes = iter(indexes)
+        next(self)
 
-    def __iter__(self: Dataset) -> Iterator[tuple[ArrayND, ...]]:
+    def __iter__(self: Dataset) -> Dataset:
         """Loops over the data in accordance to the indexes."""
-        for i in self._indexes:
-            yield tuple([x[i] for i in self.data])
+        return self
+
+    def __next__(self: Dataset) -> Sample:
+        """
+        Update and return the next sample.
+
+        Example
+        -------
+        >>> sample = next(batch.testing)
+        >>> assert sample == batch.testing.sample
+        """
+        i = next(self._indexes)
+        self.sample = tuple([x[i] for i in self.data])
+        return self.sample
 
 
 class Batch(ABC):
@@ -25,57 +56,67 @@ class Batch(ABC):
     Abstract Base Class for storing and iterating over batches.
 
     Datasets are also automatically split up into training, testing, and validation.
+    They can be looped over, and the current sample 
     """
     training: Dataset
     testing: Dataset
     validation: Dataset
 
-    def set_data(
+    def __init__(
         self: Batch,
-        training: tuple[ArrayND, ...],
-        testing: tuple[ArrayND, ...] = None,
-        validation: tuple[ArrayND, ...] = None,
+        training: Sample,
+        testing: Sample = None,
+        validation: Sample = None,
+        /,
+        *,
         test_percent: float = 0.15,
         validate_percent: float = 0.15,
     ) -> None:
         # Type-check the training data.
-        if not isinstance(training, tuple):
-            raise TypeError(f"training must be a tuple, not {type(training).__name__}")
-        elif not all(isinstance(x, ArrayND) for x in training):
-            raise TypeError("training must be a tuple of numpy arrays")
+        validate(training)
         # Shuffle the training data.
-        elif len(training) > 0:
+        if len(training) > 0:
             shuffle_indexes = np.random.permutation(len(training[0]))
             training = tuple([x[shuffle_indexes] for x in training])
         # Extract testing data from the training data if necessary.
         if testing is None:
             validate_percent /= 1 - test_percent
-            testing = tuple([x[:int(test_percent * len(training))] for x in training])
-            training = tuple([x[int(test_percent * len(training)):] for x in training])
+            testing, training = split(training, test_percent)
         # Type-check the testing data.
-        elif not isinstance(testing, tuple):
-            raise TypeError(f"testing must be a tuple, not {type(testing).__name__}")
-        elif not all(isinstance(x, ArrayND) for x in testing):
-            raise TypeError("testing must be a tuple of numpy arrays")
+        validate(testing)
         # Extract validation data from the training data if necessary.
         if validation is None:
-            validation = tuple([x[:int(validate_percent * len(training))] for x in training])
-            training = tuple([x[int(validate_percent * len(training)):] for x in training])
+            validation, training = split(training, validate_percent)
         # Type-check the validation data.
-        elif not isinstance(validation, tuple):
-            raise TypeError(f"validation must be a tuple, not {type(validation).__name__}")
-        elif not all(isinstance(x, ArrayND) for x in validation):
-            raise TypeError("testing must be a tuple of numpy arrays")
+        validate(validation)
         # Convert the data into datasets.
         self.training = Dataset(*training, indexes=self.__indexes__(training))
         self.testing = Dataset(*testing, indexes=self.__indexes__(testing))
         self.validation = Dataset(*validation, indexes=self.__indexes__(validation))
 
-    def __iter__(self: Batch) -> Iterator[tuple[ArrayND, ...]]:
-        """Loop through the training dataset repeatedly."""
-        return iter(self.training)
+    def __iter__(self: Batch) -> Batch:
+        """
+        Loop through the training dataset.
+
+        Example
+        -------
+        >>> for sample in batch:
+        ...     print(sample)
+        """
+        return self
+
+    def __next__(self: Batch) -> Sample:
+        """
+        Update the training sample.
+
+        Example
+        -------
+        >>> sample = next(batch)
+        >>> assert sample == batch.training.sample
+        """
+        return next(self.training)
 
     @abstractmethod
-    def __indexes__(self: Batch, data: ArrayND) -> Union[int, slice, tuple[int, ...]]:
+    def __indexes__(self: Batch, data: Sample) -> Union[int, slice, tuple[int, ...]]:
         """Compute indexes to loop over."""
         raise NotImplementedError
