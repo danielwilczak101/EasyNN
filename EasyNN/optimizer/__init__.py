2d076c63d8d38c7eddc9920ad56df34ae245d746
Daniel Wilczak <44122838+danielwilczak101@users.noreply.github.com>
2021-09-26 01:24:01 -0400

    Add files via upload

---
+++ EasyNN/optimizer/__init__.py
@@ -0,0 +1,4 @@
+from EasyNN.optimizer.adagrad import Optimizer_Adagrad
+from EasyNN.optimizer.adam import Optimizer_Adam
+from EasyNN.optimizer.RMSprop import Optimizer_RMSprop
+from EasyNN.optimizer.SGD import Optimizer_SGD
