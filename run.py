03f2b119aaab99dbc4eac378352dc7d7170f9488
danielwilczak101 <44122838+danielwilczak101@users.noreply.github.com>
2021-09-26 04:00:15 -0400

    Reenabled gitignore for dataset files.

--- run.py
+++ run.py
@@ -1,8 +1,8 @@
-from EasyNN.dataset.mnist.fashion import trained_model, dataset, show
+from EasyNN.dataset.mnist.fashion import trained_model, show, dataset
 
 x1,y1,x2,y2 = dataset
 
-user_image = x2[0]
+user_image = x2[1]
 
 show(user_image)
 
