6253496aeb2ba3a0b1c4f59b0be45c4bbcd1e9ba
danielwilczak101 <44122838+danielwilczak101@users.noreply.github.com>
2021-09-26 03:52:32 -0400

    Fixed trained_models file name.

--- run.py
+++ run.py
@@ -1,4 +1,4 @@
-from EasyNN.dataset.mnist.fashion import model, dataset, show
+from EasyNN.dataset.mnist.fashion import trained_model, dataset, show
 
 x1,y1,x2,y2 = dataset
 
@@ -7,4 +7,4 @@ user_image = x2[0]
 show(user_image)
 
 # Returns model
-print(model(user_image))
+print(trained_model(user_image))
