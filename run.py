6cf5d0af0d77f8dc0070a6d62ee7f91cc3e7a8fb
danielwilczak101 <44122838+danielwilczak101@users.noreply.github.com>
2021-10-01 04:08:53 -0400

    Fixing fashion dataset while implimenting new download and load code.

--- run.py
+++ run.py
@@ -1,7 +1,7 @@
-from EasyNN.dataset.mnist.number import trained_model, dataset, trained_model
+from EasyNN.dataset.mnist.fashion import  dataset
 import numpy as np
+
 
 # Downloads dataset to computer
 train_data,train_labels,test_data,test_labels = dataset
 
-trained_model(train_data[0])
