07298880570f64a4771f26f20211a5c942d46feb
danielwilczak <danielwilczak101@gmail.com>
2021-10-04 23:45:32 -0400

    Adding so i can finish in 10 mins

--- EasyNN/dataset/mnist/number.py
+++ EasyNN/dataset/mnist/number.py
@@ -1,9 +1,10 @@
 import gzip
 import pickle
-from os.path import exists
-from urllib import request
 import matplotlib.pyplot as plt
 import numpy as np
+from os.path import exists
+from urllib import request
+from PIL import Image, ImageOps
 
 from EasyNN.dataset.common import *
 from EasyNN.model.network import *
@@ -62,7 +63,7 @@ def model(user_image) -> int:
         x1, y1, x2, y2 = load(data_filename)
         # Train the model
         model.train(x1, y1, validation_data=(x2, y2), epochs=100)
-        model.evaluate(x1,y1)
+        model.evaluate(x2,y2)
         # If you want to save your model
         model.save_parameters(model_filename)
 
@@ -74,6 +75,81 @@ def model(user_image) -> int:
     predictions = model.output_layer_activation.predictions(confidences)
     # Get label name from label index
     return dataset_labels[predictions[0]]
+
+
+
+def preprocess(image_path:str, rotate: int = 0, offset: int = 30) -> list[int]:
+    """Used for taking in a user image and preprocessing 
+    it to look similar to the 28x28  dataset image.
+
+    Args:
+        image_path: The file path where the image can be found.
+        rotate: If the image needs to be rotated by a multiple of 90 degrees.
+            Ex. rotate = 1 -> rotate image 90 degrees
+            Ex. rotate = 3 -> rotate image 270 degrees
+        offset: How much addition pixel strength you want to remove from the array.
+            Ex. original = [10,20,30] if offest = 5 then new_array = [5,15,25]  
+    
+    Returns:
+        An image that has been preprocessed and is of array shape [1,784].
+
+    Raises:
+        If the file doesnt exist then tell the user you cant find the image.
+     """
+
+    try: 
+        # We opened the image and converted to grey scale.
+        user_img = Image.open(image_path).convert('L')
+        # We inverted the color to look like our dataset images.
+        user_img = ImageOps.invert(user_img)
+        # Resized the large image so it's shape is like our dataset and rotate using k.
+        user_img = np.rot90(np.array(user_img.resize([28, 28])), k=rotate)
+        # Filtered out all the grey noise in our image using the mean value.
+        user_img = np.where(user_img < np.mean(user_img) + offset,0,user_img)
+        # Reshape so its like a dataset image.
+        return user_img.reshape((1,784))
+    except FileNotFoundError:
+        print("File not found.")
+
+
+
+def show(user_image:list[int], image_type: str = None) -> None:
+    """Show the image as either array data or a matplotlib image.
+    
+    Args:
+        user_image: Users image or image from dataset.
+        image_type: Tells whether to show the image as an image or print out.
+            image: Show as matplotlib image. (default use.)
+            array: Show as numpy formated array print out.
+
+    Returns:
+        Either an matplotlib graphed image or numpy print of the image pixel values.
+
+    TODO:
+        Rasises:
+            Check to make sure the image is the correct size of either [28,28] or [1,784]
+                If its not tell them to try using preprocessing()
+                to change it to the proper size. 
+    """
+
+
+    if image_type is None or image_type == "image":
+        try:
+            plt.imshow(user_image.reshape((28, 28)), cmap='gray')
+            plt.show()
+        except ValueError:
+            print(
+                "Array not in the correct size of [1,784] or [28,28]. Try, \n \
+                    \
+                    image = preprocess(file_path)\n \
+                    or \n \
+                    image = resize(image, (28, 28)) \n "
+            )
+
+    elif image_type == "array":
+        np.set_printoptions(linewidth=114)
+        print(user_image)
+        np.set_printoptions(linewidth=75)
 
 
 def __getattr__(name):
