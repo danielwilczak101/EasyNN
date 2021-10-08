b0df7e05ae709f565d750e424ccc0c0cdbcdfd4b
SimpleArt <71458112+SimpleArt@users.noreply.github.com>
2021-10-08 18:43:45 -0400

    added basic default learning rate decay

--- EasyNN/optimizer/gradient_descent.py
+++ EasyNN/optimizer/gradient_descent.py
@@ -11,4 +11,4 @@ class GradientDescent(Optimizer):
 
     def on_training_start(self: GradientDescent, model: EasyNN.model.abc.Model) -> None:
         model.sample_derivatives(*model.training.sample)
-        model.parameters -= self.lr * model.derivatives
+        model.parameters -= self.lr / (1 + 1e-2 * model.training.iteration) ** 0.3 * model.derivatives
