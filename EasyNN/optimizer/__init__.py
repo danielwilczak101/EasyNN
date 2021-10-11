60823b01f2909c7a691b7b23c1e5fead1d283544
SimpleArt <71458112+SimpleArt@users.noreply.github.com>
2021-10-11 19:32:38 -0400

    implemented nesterov optimizer

--- EasyNN/optimizer/__init__.py
+++ EasyNN/optimizer/__init__.py
@@ -2,3 +2,4 @@ from EasyNN.optimizer.abc import Optimizer
 from EasyNN.optimizer.adam import Adam
 from EasyNN.optimizer.gradient_descent import GradientDescent
 from EasyNN.optimizer.momentum_descent import MomentumDescent
+from EasyNN.optimizer.nesterov import Nesterov
