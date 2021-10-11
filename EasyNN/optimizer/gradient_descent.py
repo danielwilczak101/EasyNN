4b683acb013b975ac754de7980deba5b75026d62
SimpleArt <71458112+SimpleArt@users.noreply.github.com>
2021-10-11 19:29:34 -0400

    added optional derivatives argument

--- EasyNN/optimizer/gradient_descent.py
+++ EasyNN/optimizer/gradient_descent.py
@@ -3,11 +3,14 @@ TODO: Not complete.
 """
 from __future__ import annotations
 from EasyNN.optimizer.abc import Optimizer
+from EasyNN.typing import ArrayND
 import EasyNN.model.abc
 
 
 class GradientDescent(Optimizer):
     """Gradient Descent uses parameters -= lr * derivatives."""
 
-    def on_training_start(self: GradientDescent, model: EasyNN.model.abc.Model) -> None:
-        model.parameters -= model._optimizer_lr / (1 + 1e-2 * model.training.iteration) ** 0.3 * self.get_derivatives(model)
+    def on_training_start(self: GradientDescent, model: EasyNN.model.abc.Model, derivatives: ArrayND = None) -> None:
+        if derivatives is None:
+            derivatives = self.get_derivatives(model)
+        model.parameters -= model._optimizer_lr / (1 + 1e-2 * model.training.iteration) ** 0.3 * derivatives
