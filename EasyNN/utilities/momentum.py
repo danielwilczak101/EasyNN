b6c4ef296b5763f2ee209333ef9ef41738b65552
SimpleArt <71458112+SimpleArt@users.noreply.github.com>
2021-10-11 19:33:51 -0400

    renamed variable to be consistent

--- EasyNN/utilities/momentum.py
+++ EasyNN/utilities/momentum.py
@@ -7,23 +7,23 @@ Array = TypeVar("Array", bound=ArrayND[float])
 
 class Momentum(Generic[Array]):
     """Basic implementation of an exponentially moving average used for momentum."""
-    _momentum: Array = 0.0
     _weight: float = 0.0
+    _value: Array = 0.0
     lr: float
 
     @property
     def value(self: Momentum[Array]) -> Array:
-        return self._momentum / self._weight
+        return self._value / self._weight
 
     @value.setter
     def value(self: Momentum[Array], value: Array) -> None:
         self._weight += self.lr * (1 - self._weight)
-        self._momentum += self.lr * (value - self._momentum)
+        self._value += self.lr * (value - self._value)
 
     def __init__(self: Momentum[Array], lr: float = 1e-2) -> None:
         self.lr = lr
 
     def update(self: Momentum[Array], value: Array) -> Array:
         """Update and return the momentum value."""
-        self.momentum = value
-        return self.momentum
+        self.value = value
+        return self.value
