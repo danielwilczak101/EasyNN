fb104a9427aa15d45df2c38f71974c2b2c152f8e
danielwilczak101 <44122838+danielwilczak101@users.noreply.github.com>
2021-10-14 19:09:07 -0400

    changed data to images.

--- dan.py
+++ dan.py
@@ -1,6 +1,6 @@
 from EasyNN.dataset.mnist.number import dataset
 from examples.mnist.number.trained import model
 
-data, labels = dataset
+images, labels = dataset
 
-print(model.accuracy(data,labels))
+print(model.accuracy(images,labels))
