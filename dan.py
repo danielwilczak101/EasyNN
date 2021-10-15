427b9e69aa5e82a1e1215f351db06a56d75f74c2
danielwilczak101 <44122838+danielwilczak101@users.noreply.github.com>
2021-10-15 18:47:51 -0400

    Added fashion dataset.

--- dan.py
+++ dan.py
@@ -1,32 +1,13 @@
-from EasyNN.examples.mnist.number.trained import model
-from EasyNN.examples.mnist.number import show
-from EasyNN.examples.mnist.number.data import dataset
-from EasyNN.utilities.download import download
+from EasyNN.examples.mnist.fashion.trained import model
+from EasyNN.examples.mnist.fashion.data import dataset
+from EasyNN.examples.mnist.fashion import show
+
 from EasyNN.utilities.image.preprocess import image
-from EasyNN.utilities.image.compare import compare
 from EasyNN.utilities.download import download
 
 images, labels = dataset
 
-download("six.png","https://github.com/danielwilczak101/EasyNN/raw/datasets/mnist/number/examples/six.png")
-
-print(model.accuracy(images, labels))
-
-format_options = dict(
-    grayscale=True,
-    invert=True,
-    process=True,
-    contrast=35,
-    resize=(28, 28),
-    rotate=0,
-)
-
-# Converting your image into the correct format for the mnist number dataset.
-image = image("six.png").format(**format_options)
-
-show(image)
-
-print(model.classify(image))
+print(model.accuracy())
 
 
 
--- jack.py
+++ jack.py
@@ -1,1 +1,16 @@
+from EasyNN.examples.mnist.fashion.untrained import model
+from EasyNN.utilities.image.preprocess import image
+from EasyNN.utilities.download import download
+from EasyNN.utilities.parameters import save
+from EasyNN.examples.mnist.number.data import dataset
+from EasyNN.examples.mnist.number import show
 
+# Once training has been finished save the model to use later
+@model.on_testing_end
+def save_data(model):
+    save("fashion_model.npz", **model.get_arrays())
+    
+# Train the model.
+model.train()
+images, labels = dataset
+print(model.accuracy(images, labels))
