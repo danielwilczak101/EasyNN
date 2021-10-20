a1a9e4da88b86c06b934bcba0c772b7c5d68c36c
danielwilczak <danielwilczak101@gmail.com>
2021-10-20 03:48:23 -0400

    Update linked for fashion trained model files.

--- EasyNN/examples/mnist/fashion/trained.py
+++ EasyNN/examples/mnist/fashion/trained.py
@@ -2,8 +2,8 @@ from EasyNN.model import Model
 from EasyNN.utilities.download import download
 
 # Required for the trained model.
-download("fashion_parameters.npz","#")
-download("fashion_structure.pkl","#")
+download("fashion_parameters.npz","https://github.com/danielwilczak101/EasyNN/raw/datasets/mnist/fashion/fashion_parameters.npz")
+download("fashion_structure.pkl","https://github.com/danielwilczak101/EasyNN/raw/datasets/mnist/fashion/fashion_structure.pkl")
 
 # Load the saved model
 model = Model.load("fashion")
--- dan.py
+++ dan.py
@@ -1,18 +1,7 @@
-from EasyNN.examples.mnist.fashion.untrained import model
-from EasyNN.examples.mnist.fashion.data import dataset
-from EasyNN.model import Model
+from EasyNN.examples.mnist.number.trained import model
+from EasyNN.examples.mnist.number.data import dataset
 
 images, labels = dataset
 
-# Train the model.
-model.train()
-model.save("fashion")
-
-# Check the models accuracy
-print(model.accuracy(images, labels))
-
-# Load the saved model
-model = Model.load("fashion")
-
 # Check the model again so its teh same.
-print(model.accuracy(images, labels))
+print(model.classify(images[1]))
