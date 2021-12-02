0bd7085e8c620ae9a2ffbfd3f546bfa51f317182
danielwilczak101 <44122838+danielwilczak101@users.noreply.github.com>
2021-12-02 13:46:29 -0500

    Cifar10 changes

--- dan.py
+++ dan.py
@@ -1,21 +1,7 @@
-"""
-from EasyNN.model import Model
 from EasyNN.examples.cifar10.untrained import model
+from EasyNN.examples.cifar10.data import dataset
 
-model.train()
-model.save("cifar10")
-model = Model.load("cifar10")
-
-"""
-
-from EasyNN.examples.mnist.fashion.trained import model
-from EasyNN.examples.mnist.fashion.data import dataset
-
-images, labels = dataset
-
-# Classify what the second image is in the dataset.
-print(model.classify(images[0]))
-
-model.show(images[0])
-
-model.accuracy(dataset)
+try:
+    model.train()
+except KeyboardInterrupt:
+    model.save("cifar10")
