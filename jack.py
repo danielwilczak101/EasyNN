264dc04fbecfce644b361cf75b4b073a0b53d609
Daniel Wilczak <44122838+danielwilczak101@users.noreply.github.com>
2022-01-08 14:58:03 -0600

    Update jack.py

--- jack.py
+++ jack.py
@@ -1,7 +1,9 @@
 from EasyNN.examples.cifar10.trained import model
 from EasyNN.examples.cifar10.data import dataset
 
-images, labels = dataset
 
-# Show the image
-model.show(images[2])
+def hello():
+  images, labels = dataset
+
+  # Show the image
+  model.show(images[2])
