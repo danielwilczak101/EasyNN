ad09ca00e9df1b2520497de1a894299f8f6f765c
Daniel Wilczak <44122838+danielwilczak101@users.noreply.github.com>
2022-08-03 23:24:43 -0500

    Update scrape.py

--- EasyNN/utilities/scrape/scrape.py
+++ EasyNN/utilities/scrape/scrape.py
@@ -1,9 +1,11 @@
 import hashlib
 from PIL import Image
-import io, os
+import io
+import os
 import requests
 import time
 from selenium import webdriver
+from selenium.webdriver.common.by import By
 
 """
 Code downloaded from:
@@ -11,11 +13,17 @@ Code downloaded from:
     Author:Fabian Bosler
 """
 
-def fetch_image_urls(query:str, max_links_to_fetch:int, wd:webdriver, sleep_between_interactions:int=1):
+
+def fetch_image_urls(
+    query: str,
+    max_links_to_fetch: int,
+    wd: webdriver,
+    sleep_between_interactions: int = 1,
+):
     def scroll_to_end(wd):
         wd.execute_script("window.scrollTo(0, document.body.scrollHeight);")
-        time.sleep(sleep_between_interactions)    
-    
+        time.sleep(sleep_between_interactions)
+
     # build the google query
     search_url = "https://www.google.com/search?safe=off&site=&tbm=isch&source=hp&q={q}&oq={q}&gs_l=img"
 
@@ -29,11 +37,14 @@ def fetch_image_urls(query:str, max_links_to_fetch:int, wd:webdriver, sleep_betw
         scroll_to_end(wd)
 
         # get all image thumbnail results
-        thumbnail_results = wd.find_elements_by_css_selector("img.Q4LuWd")
+        thumbnail_results = wd.find_element(By.CSS_SELECTOR, "img.Q4LuWd")
+
         number_results = len(thumbnail_results)
-        
-        print(f"Found: {number_results} search results. Extracting links from {results_start}:{number_results}")
-        
+
+        print(
+            f"Found: {number_results} search results. Extracting links from {results_start}:{number_results}"
+        )
+
         for img in thumbnail_results[results_start:number_results]:
             # try to click every thumbnail such that we can get the real image behind it
             try:
@@ -43,11 +54,14 @@ def fetch_image_urls(query:str, max_links_to_fetch:int, wd:webdriver, sleep_betw
                 print(e)
                 continue
 
-            # extract image urls    
-            actual_images = wd.find_elements_by_css_selector('img.n3VNCb')
+            # extract image urls
+            actual_images = wd.find_element(By.CSS_SELECTOR, "img.n3VNCb")
+
             for actual_image in actual_images:
-                if actual_image.get_attribute('src') and 'http' in actual_image.get_attribute('src'):
-                    image_urls.add(actual_image.get_attribute('src'))
+                if actual_image.get_attribute(
+                    "src"
+                ) and "http" in actual_image.get_attribute("src"):
+                    image_urls.add(actual_image.get_attribute("src"))
 
             image_count = len(image_urls)
 
@@ -55,10 +69,12 @@ def fetch_image_urls(query:str, max_links_to_fetch:int, wd:webdriver, sleep_betw
                 print(f"Found: {len(image_urls)} image links, done!")
                 break
         else:
-            print("Found:", len(image_urls), "image links, looking for more ...")
+            print("Found:", len(image_urls),
+                  "image links, looking for more ...")
             time.sleep(30)
-            #return
-            load_more_button = wd.find_element_by_css_selector(".mye4qd")
+            # return
+            load_more_button = wd.find_element(By.CSS_SELECTOR, ".mye4qd")
+
             if load_more_button:
                 wd.execute_script("document.querySelector('.mye4qd').click();")
 
@@ -67,7 +83,8 @@ def fetch_image_urls(query:str, max_links_to_fetch:int, wd:webdriver, sleep_betw
 
     return image_urls
 
-def persist_image(folder_path:str,url:str,index:int):
+
+def persist_image(folder_path: str, url: str, index: int):
     try:
         image_content = requests.get(url).content
 
@@ -76,25 +93,31 @@ def persist_image(folder_path:str,url:str,index:int):
 
     try:
         image_file = io.BytesIO(image_content)
-        image = Image.open(image_file).convert('RGB')
-        file_path = os.path.join(folder_path, str(index) + '.jpg')
-        with open(file_path, 'wb') as f:
+        image = Image.open(image_file).convert("RGB")
+        file_path = os.path.join(folder_path, str(index) + ".jpg")
+        with open(file_path, "wb") as f:
             image.save(f, "JPEG", quality=85)
         print(f"SUCCESS - saved {url} - as {file_path}")
     except Exception as e:
         print(f"ERROR - Could not save {url} - {e}")
 
-def scrape_google(search_term:str, driver_path='./chromedriver', target_path='./images', count=5):
+
+def scrape_google(
+    search_term: str,
+    driver_path: str = "./chromedriver",
+    target_path: str = "./images",
+    count: int = 5,
+):
     """
     Used for scraping images from google using the chromedriver.
 
     Requirement:
-        chromedriver - 
+        chromedriver -
             1. Install Google Chrome (skip if its already installed)
-            2. Identify your Chrome version. In chrome go to  "About Google Chrome". I 
+            2. Identify your Chrome version. In chrome go to  "About Google Chrome". I
             currently have version 77.0.3865.90 (my main version is thus 77, the number
             before the first dot).
-            
+
             https://chromedriver.chromium.org/downloads
 
             Download your corresponding ChromeDriver from the link provided for your main
@@ -110,9 +133,9 @@ def scrape_google(search_term:str, driver_path='./chromedriver', target_path='./
 
         >>> scrape_google("dog", target_path='./my_new_folder')
         Opens browser and downloads 5 images of dog and put it into a folder named "my_new_folder".
-        
+
     Function parameters:
-        
+
         search_term: The term you want to scrape google images for.
 
         count: Number of image to scrape from google.
@@ -125,16 +148,18 @@ def scrape_google(search_term:str, driver_path='./chromedriver', target_path='./
     Return:
         Images folder with sub folder of the search term with images inside of there.
 
-    
+
     """
-    target_folder = os.path.join(target_path,'_'.join(search_term.lower().split(' ')))
+    target_folder = os.path.join(
+        target_path, "_".join(search_term.lower().split(" ")))
 
     if not os.path.exists(target_folder):
         os.makedirs(target_folder)
 
     with webdriver.Chrome(executable_path=driver_path) as wd:
-        res = fetch_image_urls(search_term, count, wd=wd, sleep_between_interactions=0.5)
+        res = fetch_image_urls(
+            search_term, count, wd=wd, sleep_between_interactions=0.5
+        )
 
     for index, elem in enumerate(res):
-        persist_image(target_folder,elem,index)
-
+        persist_image(target_folder, elem, index)
